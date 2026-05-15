4f2150e2773a844043663d4e4ad19eaf43c408c1
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-15 22:21:09 +0200

    feature(models): Create store object that represents single source of truth for data

---
+++ TheMet/Models/TheMetStore.swift
@@ -0,0 +1,29 @@
+//
+//  TheMetStore.swift
+//  TheMet
+//
+//  Created by Damian Ogórek on 15/05/2026.
+//
+
+import Combine
+import Foundation
+
+//class TheMetStore: ObservableObject {
+//  @Published var objects: [Object] = []
+//
+//  init() {
+//    #if DEBUG
+//    createDevData()
+//    #endif
+//  }
+//}
+
+class TheMetStore: ObservableObject {
+    @Published var objects: [Object] = []
+
+    init() {
+        #if DEBUG
+        createDevData()
+        #endif
+    }
+}
