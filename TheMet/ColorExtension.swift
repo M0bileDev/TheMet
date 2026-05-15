8a768b7d9cfa700df8ad77de191dfc01d7d36ef6
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-15 22:01:50 +0200

    feature(extensions): create Color extensions for background and foreground colors

---
+++ TheMet/ColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtension.swift
+//  TheMet
+//
+//  Created by Damian Ogórek on 15/05/2026.
+//
+
+import SwiftUI
+
+extension Color {
+    static let metBackgroundColor = Color("met-background")
+    static let metForegroundColor = Color("met-foreground")
+}
