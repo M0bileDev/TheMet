e8fa7ca3e072646ede7db8ad0f5b7e52f0976a98
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-15 21:48:29 +0200

    Initial Commit

---
+++ TheMet/TheMetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMetApp.swift
+//  TheMet
+//
+//  Created by Damian Ogórek on 15/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct TheMetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
