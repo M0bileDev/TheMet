fec839a48654f2d006cef13e96c24d50a0b8e50b
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-15 22:42:41 +0200

    feature(views): cerate view that represents placeholder

---
+++ TheMet/Views/PlaceholderView.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceholderView.swift
+//  TheMet
+//
+//  Created by Damian Ogórek on 15/05/2026.
+//
+
+import SwiftUI
+
+struct PlaceholderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaceholderView()
+}
