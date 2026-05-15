81988578392823c40044f589cad8c3efea50b3e9
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-15 22:40:30 +0200

    feature(views): create view that represents object
    Added property that represents object model

--- TheMet/Views/ObjectView.swift
+++ TheMet/Views/ObjectView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct ObjectView: View {
+
+    let object: Object
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text( /*@START_MENU_TOKEN@*/"Hello, World!" /*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ObjectView()
+    ObjectView(object: initialObjects[0])
 }
