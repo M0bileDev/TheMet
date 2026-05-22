db06b7bbf0fd078d022862358dbebcf0f809c45f
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-22 21:22:34 +0200

    feature(views): add reminder of what user searched for

--- TheMet/Views/ContentView.swift
+++ TheMet/Views/ContentView.swift
@@ -15,55 +15,63 @@ struct ContentView: View {
 
     var body: some View {
         NavigationStack {
-            List(store.objects, id: \.objectId) { object in
-                if !object.isPublicDomain,
-                    let url = URL(string: object.objectURL)
-                {
-                    NavigationLink(value: url) {
-                        WebIndicatorView(title: object.title)
-                    }
-                    .listRowBackground(Color.metBackground)
-                    .foregroundStyle(.white)
-                } else {
-                    NavigationLink(value: object) {
-                        Text(object.title)
-                    }
-                    .listRowBackground(Color.metForeground)
-                }
+            VStack {
+                Text("You searched for '\(query)'")
+                    .padding(10)
+                    .background(Color.metForeground)
+                    .cornerRadius(10)
+                    .padding()
 
+                List(store.objects, id: \.objectId) { object in
+                    if !object.isPublicDomain,
+                        let url = URL(string: object.objectURL)
+                    {
+                        NavigationLink(value: url) {
+                            WebIndicatorView(title: object.title)
+                        }
+                        .listRowBackground(Color.metBackground)
+                        .foregroundStyle(.white)
+                    } else {
+                        NavigationLink(value: object) {
+                            Text(object.title)
+                        }
+                        .listRowBackground(Color.metForeground)
+                    }
+
+                }
+                .navigationTitle("The Met")
+                .toolbar {
+                    Button("Search the Met") {
+                        query = ""
+                        showQueryField = true
+                    }
+                    .foregroundColor(Color.metBackground)
+                    .padding(.horizontal)
+                    .shadow(color: Color.red.opacity(0.5), radius: 6)
+                }
+                .alert(
+                    "Search the Met",
+                    isPresented: $showQueryField,
+                    actions: {
+                        TextField("Seatch the Met", text: $query)
+                        Button("Search") {}
+                    }
+                )
+                .navigationDestination(
+                    for: URL.self,
+                    destination: { url in
+                        SafariView(url: url)
+                            .navigationBarTitleDisplayMode(.inline)
+                            .ignoresSafeArea()
+                    }
+                )
+                .navigationDestination(
+                    for: Object.self,
+                    destination: { object in
+                        ObjectView(object: object)
+                    }
+                )
             }
-            .navigationTitle("The Met")
-            .toolbar {
-                Button("Search the Met") {
-                    query = ""
-                    showQueryField = true
-                }
-                .foregroundColor(Color.metBackground)
-                .padding(.horizontal)
-                .shadow(color: Color.red.opacity(0.5), radius: 6)
-            }
-            .alert(
-                "Search the Met",
-                isPresented: $showQueryField,
-                actions: {
-                    TextField("Seatch the Met", text: $query)
-                    Button("Search") {}
-                }
-            )
-            .navigationDestination(
-                for: URL.self,
-                destination: { url in
-                    SafariView(url: url)
-                        .navigationBarTitleDisplayMode(.inline)
-                        .ignoresSafeArea()
-                }
-            )
-            .navigationDestination(
-                for: Object.self,
-                destination: { object in
-                    ObjectView(object: object)
-                }
-            )
         }
     }
 }
