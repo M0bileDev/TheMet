c7580e5d67eb9b0e022a64694e6e4f09ea639c9e
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-25 22:29:26 +0200

    feature(service): provide implemention of call and decode response data into ObjectIds type

--- TheMet/Service/TheMetService.swift
+++ TheMet/Service/TheMetService.swift
@@ -28,7 +28,28 @@ struct TheMetService {
         guard let url = urlComponents.url else { return nil }
         let request = URLRequest(url: url)
 
-        return nil
+        let (data, response) = try await session.data(for: request)
+
+        guard let httpUrlResponse = response as? HTTPURLResponse else {
+            print("getObjectIDs: response is not HTTPURLResponse")
+            return nil
+        }
+
+        guard (200..<300).contains(httpUrlResponse.statusCode) else {
+            print(
+                "getObjectIDs incorrect, status code: \(httpUrlResponse.statusCode)"
+            )
+            return nil
+        }
+
+        do {
+            objectIds = try decoder.decode(ObjectIds.self, from: data)
+        } catch {
+            print("Decoder error: \(error)")
+            return nil
+        }
+
+        return objectIds
     }
 
     func getObject(objectId: Int) async throws -> Object? {
