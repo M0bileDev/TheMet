a29df65fc73864d540b877895c38a29afd1824ad
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-27 19:12:06 +0200

    feature(widget): create widget configuration

---
+++ TheMetWidget/TheMetWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TheMetWidgetBundle.swift
+//  TheMetWidget
+//
+//  Created by Damian Ogórek on 27/05/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TheMetWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TheMetWidget()
+    }
+}
