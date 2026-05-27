fe758ee0aeaeb094001f608710619f798240de58
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-27 19:17:39 +0200

    refactor(widget): change name and description

--- TheMetWidget/TheMetWidget.swift
+++ TheMetWidget/TheMetWidget.swift
@@ -5,26 +5,36 @@
 //  Created by Damian Ogórek on 27/05/2026.
 //
 
-import WidgetKit
 import SwiftUI
+import WidgetKit
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), emoji: "😀")
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+    func getSnapshot(
+        in context: Context,
+        completion: @escaping (SimpleEntry) -> Void
+    ) {
         let entry = SimpleEntry(date: Date(), emoji: "😀")
         completion(entry)
     }
 
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    func getTimeline(
+        in context: Context,
+        completion: @escaping (Timeline<Entry>) -> Void
+    ) {
         var entries: [SimpleEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+        for hourOffset in 0..<5 {
+            let entryDate = Calendar.current.date(
+                byAdding: .hour,
+                value: hourOffset,
+                to: currentDate
+            )!
             let entry = SimpleEntry(date: entryDate, emoji: "😀")
             entries.append(entry)
         }
@@ -33,9 +43,9 @@ struct Provider: TimelineProvider {
         completion(timeline)
     }
 
-//    func relevances() async -> WidgetRelevances<Void> {
-//        // Generate a list containing the contexts this widget is relevant in.
-//    }
+    //    func relevances() async -> WidgetRelevances<Void> {
+    //        // Generate a list containing the contexts this widget is relevant in.
+    //    }
 }
 
 struct SimpleEntry: TimelineEntry {
@@ -43,7 +53,7 @@ struct SimpleEntry: TimelineEntry {
     let emoji: String
 }
 
-struct TheMetWidgetEntryView : View {
+struct TheMetWidgetEntryView: View {
     var entry: Provider.Entry
 
     var body: some View {
@@ -61,7 +71,12 @@ struct TheMetWidget: Widget {
     let kind: String = "TheMetWidget"
 
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+        StaticConfiguration(
+            kind: kind,
+            provider:
+                //timeline, snapshot and placeholder entries
+                Provider()
+        ) { entry in
             if #available(iOS 17.0, *) {
                 TheMetWidgetEntryView(entry: entry)
                     .containerBackground(.fill.tertiary, for: .widget)
@@ -71,8 +86,8 @@ struct TheMetWidget: Widget {
                     .background()
             }
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("The Met")
+        .description("View objects from the Metropolitan Museum.")
     }
 }
 
